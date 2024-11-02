99127a4b7a362b0b75709ccfe7880a21e482817c
GiaBaoAnh04 <giabaoanh3003@gmail.com>
2024-11-02 14:36:12 +0700

    add ?

--- components/header/HeaderNoButton.tsx
+++ components/header/HeaderNoButton.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="background-light700_dark400 flex w-full flex-col">
       <p className="text-dark100_light500 text-[32px]">Good Morning, Meo</p>
-      <p className="text-[16px] text-primary-100">Have a nice day!</p>
+      <p className="text-[16px] text-primary-100">Have a nice day!?!</p>
     </div>
   );
 };
